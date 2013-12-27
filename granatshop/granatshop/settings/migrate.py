a762e934fad21ab567e5ec479b5c61784500331e
temaput <putilkin@gmail.com>
2013-12-27 12:57:56 +0400

    removed migrate settings

--- granatshop/granatshop/settings/migrate.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-from .dev import *
-
-# need to remove theese to prevent south from being to cautious
-INSTALLED_APPS.remove('apps.checkout')
-INSTALLED_APPS.remove('apps.shipping')
